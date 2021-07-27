30dba4e4af3aebddb9d59848cc618129fa81fe68
dr497 <anand.ratiney@gmail.com>
2021-07-27 14:59:41 +0800

    add markets

--- js/src/index.ts
+++ js/src/index.ts
@@ -5,3 +5,4 @@ export * from "./state";
 export * from "./instructions";
 export * from "./high_level";
 export * from "./types";
+export * from "./markets";
---
+++ js/src/markets.ts
@@ -0,0 +1,10 @@
+export const MARKETS = [
+  {
+    address: "475P8ZX3NrzyEMJSFHt9KCMjPpWBWGa6oNxkWcwww2BR",
+    name: "BTC-PERP",
+  },
+  {
+    address: "3ds9ZtmQfHED17tXShfC1xEsZcfCvmT8huNG79wa4MHg",
+    name: "ETH-PERP",
+  },
+];
