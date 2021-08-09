f61a2ef3cdfe7f91cc4bea893835049e0d7cfa5d
DR497 <47689875+dr497@users.noreply.github.com>
2021-08-09 18:34:59 +0800

    JS: add SOL address

--- js/src/markets.ts
+++ js/src/markets.ts
@@ -7,4 +7,8 @@ export const MARKETS = [
     address: "3ds9ZtmQfHED17tXShfC1xEsZcfCvmT8huNG79wa4MHg",
     name: "ETH-PERP",
   },
+  {
+    address: "jeVdn6rxFPLpCH9E6jmk39NTNx2zgTmKiVXBDiApXaV",
+    name: "SOL-PERP",
+  },
 ];
